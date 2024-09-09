a527945232310aa47c66a313de705d84b15fcb9a
Krittin Chuaiphikroh <krittin@42bangkok.com>
2024-09-09 14:58:30 +0900

    Initial commit
    Change-Id: I6ed362ecf0a764951396b3655707be2ac0e65b63
    Signed-off-by: Krittin Chuaiphikroh <krittin@42bangkok.com>

---
+++ src/client/kotlin/org/haruka/scanner/EntityScannerClient.kt
@@ -0,0 +1,9 @@
+package org.haruka.scanner
+
+import net.fabricmc.api.ClientModInitializer
+
+object EntityScannerClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
