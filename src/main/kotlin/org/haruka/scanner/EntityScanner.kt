435b95f375b6c7ab3c13060b9d03620103bbc12d
Krittin Chuaiphikroh <krittin@42bangkok.com>
2024-09-09 16:25:00 +0900

    EntityScanner: Update
    Change-Id: Ia19c977b7599896a7462825419e8492affe28cab
    Signed-off-by: Krittin Chuaiphikroh <krittin@42bangkok.com>

--- src/main/kotlin/org/haruka/scanner/EntityScanner.kt
+++ src/main/kotlin/org/haruka/scanner/EntityScanner.kt
@@ -1,15 +1,66 @@
 package org.haruka.scanner
 
+import com.mojang.brigadier.arguments.IntegerArgumentType
+import com.mojang.brigadier.context.CommandContext
 import net.fabricmc.api.ModInitializer
-import org.slf4j.LoggerFactory
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
+import net.minecraft.server.command.CommandManager
+import net.minecraft.server.command.ServerCommandSource
+import net.minecraft.text.Text
+import net.minecraft.util.math.Box
+import net.minecraft.util.math.ChunkPos
 
 object EntityScanner : ModInitializer {
-    private val logger = LoggerFactory.getLogger("entity-scanner")
 
 	override fun onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		logger.info("Hello Fabric world!")
+		CommandRegistrationCallback.EVENT.register { dispatcher, _, _ ->
+			dispatcher.register(
+				CommandManager.literal("scan")
+					.then(CommandManager.argument("radius", IntegerArgumentType.integer(1, 5))
+						.executes { context -> scanEntitiesInChunks(context, IntegerArgumentType.getInteger(context, "radius")) })
+					.executes { context -> scanEntitiesInChunks(context, 1) }
+			)
+		}
+	}
+
+	private fun scanEntitiesInChunks(context: CommandContext<ServerCommandSource>, radius: Int): Int {
+		val player = context.source.player
+		if (player != null) {
+			val world = player.world
+			val playerChunkPos = ChunkPos(player.blockPos)
+
+			val entityCountMap: MutableMap<String, Int> = mutableMapOf()
+
+			for (chunkXOffset in -radius..radius) {
+				for (chunkZOffset in -radius..radius) {
+					val chunkPos = ChunkPos(playerChunkPos.x + chunkXOffset, playerChunkPos.z + chunkZOffset)
+
+					val minX = chunkPos.startX.toDouble()
+					val minY = world.bottomY.toDouble()
+					val minZ = chunkPos.startZ.toDouble()
+					val maxX = chunkPos.endX.toDouble() + 1
+					val maxY = world.topY.toDouble()
+					val maxZ = chunkPos.endZ.toDouble() + 1
+
+					val box = Box(minX, minY, minZ, maxX, maxY, maxZ)
+
+					val entities = world.getOtherEntities(player, box)
+
+					for (entity in entities) {
+						val entityName = entity.type.toString()
+						entityCountMap[entityName] = entityCountMap.getOrDefault(entityName, 0) + 1
+					}
+				}
+			}
+
+			val output = StringBuilder("Entity count in your current and nearby chunks (radius: $radius):\n")
+
+			for ((entityName, count) in entityCountMap) {
+				output.append("$entityName: $count\n")
+			}
+
+			player.sendMessage(Text.literal(output.toString()), false)
+		}
+		return 1
 	}
 }
